860e7bc86a266c485d983baba1825b76c597ff09
Felix Grüning <fegrue@gmail.com>
2026-05-14 20:36:20 +0200

    added clawmachine-mqtt_connection

---
+++ lib/claw_mqtt_connection/claw_mqtt_connection.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+class ClawMqttConnection {
+public:
+  ClawMqttConnection(
+      const char *wifiSsid,
+      const char *wifiPassword,
+      const char *mqttBrokerHost,
+      uint16_t mqttBrokerPort,
+      const char *mqttClientId,
+      unsigned long reconnectIntervalMilliseconds);
+
+  void begin();
+  void maintainConnection();
+
+private:
+  bool ensureWifiConnected();
+  bool ensureMqttConnected();
+
+  const char *wifiSsid;
+  const char *wifiPassword;
+  const char *mqttBrokerHost;
+  uint16_t mqttBrokerPort;
+  const char *mqttClientId;
+  unsigned long reconnectIntervalMilliseconds;
+  unsigned long lastWifiConnectAttemptMilliseconds;
+  unsigned long lastMqttConnectAttemptMilliseconds;
+  WiFiClient networkClient;
+  PubSubClient mqttClient;
+};
