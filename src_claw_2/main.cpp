bfc38d2d3c951e3d6229cc6469624221d648f036
tarantelkorn <leonhard.daxl@gmail.com>
2026-05-15 13:36:15 +0200

    Classic Bluetooth über den ESP32 WROOM1

--- src_claw_2/main.cpp
+++ src_claw_2/main.cpp
@@ -1,57 +1,86 @@
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include "BluetoothSerial.h"
+#include "esp_bt_device.h"
+#include "esp_gap_bt_api.h"
 
-#define SCAN_TIME 5   // Sekunden
+BluetoothSerial SerialBT;
 
-BLEScan* pBLEScan;
+void btAdvertisedDeviceFound(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+  if (event == ESP_BT_GAP_DISC_RES_EVT)
+  {
+    Serial.println("----- Classic Bluetooth Gerät gefunden -----");
 
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
-  void onResult(BLEAdvertisedDevice device) override {
-    Serial.println("----- Bluetooth Gerät gefunden -----");
-
-    Serial.print("Name: ");
-    if (device.haveName()) {
-      Serial.println(device.getName().c_str());
-    } else {
-      Serial.println("(kein Name)");
-    }
+    char bda_str[18];
+    snprintf(bda_str, sizeof(bda_str),
+             "%02X:%02X:%02X:%02X:%02X:%02X",
+             param->disc_res.bda[0], param->disc_res.bda[1],
+             param->disc_res.bda[2], param->disc_res.bda[3],
+             param->disc_res.bda[4], param->disc_res.bda[5]);
 
     Serial.print("MAC: ");
-    Serial.println(device.getAddress().toString().c_str());
+    Serial.println(bda_str);
 
-    Serial.print("RSSI: ");
-    Serial.println(device.getRSSI());
+    for (int i = 0; i < param->disc_res.num_prop; i++)
+    {
+      esp_bt_gap_dev_prop_t prop = param->disc_res.prop[i];
 
-    Serial.println("------------------------------------");
+      if (prop.type == ESP_BT_GAP_DEV_PROP_EIR)
+      {
+        uint8_t len = 0;
+        uint8_t *name = esp_bt_gap_resolve_eir_data(
+            (uint8_t *)prop.val,
+            ESP_BT_EIR_TYPE_CMPL_LOCAL_NAME,
+            &len);
+
+        if (name)
+        {
+          Serial.print("Name: ");
+          Serial.write(name, len);
+          Serial.println();
+        }
+      }
+
+      if (prop.type == ESP_BT_GAP_DEV_PROP_RSSI)
+      {
+        Serial.print("RSSI: ");
+        Serial.println(*(int8_t *)prop.val);
+      }
+    }
+
+    Serial.println("--------------------------------------------");
   }
-};
+  else if (event == ESP_BT_GAP_DISC_STATE_CHANGED_EVT)
+  {
+    if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STOPPED)
+    {
+      Serial.println("Scan fertig.");
+      Serial.println("Starte neuen Scan...");
+      esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
+    }
+  }
+}
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println("ESP32 Bluetooth Scanner startet...");
+  Serial.println("Classic Bluetooth Scanner startet...");
 
-  BLEDevice::init("");
+  if (!SerialBT.begin("ESP32_BT_SCANNER"))
+  {
+    Serial.println("Bluetooth Start fehlgeschlagen!");
+    return;
+  }
 
-  pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setActiveScan(true);
-  pBLEScan->setInterval(100);
-  pBLEScan->setWindow(99);
+  esp_bt_gap_register_callback(btAdvertisedDeviceFound);
+
+  Serial.println("Starte Classic Bluetooth Inquiry...");
+  esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
 }
 
-void loop() {
-  Serial.println("Scan läuft...");
-
-  BLEScanResults results = pBLEScan->start(SCAN_TIME, false);
-
-  Serial.print("Gefundene Geräte: ");
-  Serial.println(results.getCount());
-
-  pBLEScan->clearResults();
-
-  delay(2000);
+void loop()
+{
+  delay(1000);
 }
