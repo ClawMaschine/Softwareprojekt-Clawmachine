bfab9e4d00c789161aecaa7a8025b0ba874b863f
Felix Grüning <fegrue@gmail.com>
2026-06-05 11:38:17 +0200

    added emulated_esp

--- python_server/emulated_esp/emulated.py
+++ python_server/emulated_esp/emulated.py
@@ -1,15 +1,30 @@
-from python_server.mqtt import MQTTClient
+import time
 
-from ..configuration_loader import MqttConfiguration, load_mqtt_configuration
-from ..esp.mqtt_esp_controller import MQTTEspController
+try:
+    from python_server.mqtt import MQTTClient
+    from python_server.configuration_loader import load_mqtt_configuration
+except ModuleNotFoundError:
+    from mqtt import MQTTClient
+    from configuration_loader import load_mqtt_configuration
 
-load_mqtt_configuration()
+EMULATED_ESP_NAME = "emulated_esp"
+
 mqtt_configuration = load_mqtt_configuration()
 mqtt_client = MQTTClient(
-    client_id=mqtt_configuration.client_id,
+    client_id=EMULATED_ESP_NAME,
     broker=mqtt_configuration.broker,
     port=mqtt_configuration.port,
     connect_timeout_seconds=mqtt_configuration.connect_timeout_seconds,
+    username=mqtt_configuration.username,
+    password=mqtt_configuration.password,
 )
 mqtt_client.connect()
-mqtt_client.publish(mqtt_configuration.device_added_topic, "Emulated ESP")
+mqtt_client.publish(mqtt_configuration.device_added_topic, EMULATED_ESP_NAME)
+
+uptime_topic = f"clawmachine/{EMULATED_ESP_NAME}/metadata/uptime"
+started_at = time.time()
+
+while True:
+    uptime_ms = int((time.time() - started_at) * 1000)
+    mqtt_client.publish(uptime_topic, str(uptime_ms))
+    time.sleep(1)
