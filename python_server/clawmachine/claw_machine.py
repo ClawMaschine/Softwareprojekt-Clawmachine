f420345d823301ac11c1c7316d4c69f4d57ee873
Felix Grüning <fegrue@gmail.com>
2026-07-30 13:22:16 +0200

    .

--- python_server/clawmachine/claw_machine.py
+++ python_server/clawmachine/claw_machine.py
@@ -116,7 +116,7 @@ class ClawMachine:
 
     def main_loop(self):
         while True:
-            self.mqtt_client.publish("/motor_controller/motor/command", "X:50");
+            self.mqtt_client.publish("clawmachine/motor_controller/motor/command", "X:50");
 
             time.sleep(1)
 
